14c6330c41ed27e35adabcf71e38d18bb1de07f3
megavarshiniv <megavarshinivenkatesan@gamil.com>
2026-06-18 18:15:09 +0530

    Day3 DSA Learning

---
+++ Day4/Maximum_subarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n=nums.size();
+        int max_sum=INT_MIN,sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            if(sum>max_sum){
+                max_sum=sum;
+            }
+            if(sum<0){
+                sum=0;
+            }
+        }
+
+        return max_sum;
+    }
+};
